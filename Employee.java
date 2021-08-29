98ab723ad72bbba8fc57fb45fdc60e348bdca818
amitpjha <89724452+amitpjha@users.noreply.github.com>
2021-08-29 20:25:23 +0530

    Add files via upload
    Added First version for submission of First Lab assignment

---
+++ Employee.java
@@ -0,0 +1,31 @@
+package com.greatlearning.model;
+
+public class Employee 
+{
+	private String firstName ;
+	private String lastName ;
+
+	public Employee(String firstName,String lastName )
+	{
+		this.firstName = firstName ;
+		this.lastName = lastName ;
+	}
+	public void setFirstName( String firstName)
+	{
+		this.firstName = firstName ;
+	}
+	public String getFirstName()
+	{
+		return this.firstName ;
+	}
+	public void setLastName( String lastName)
+	{
+		this.lastName = lastName ;
+	}
+	public String getLastName()
+	{
+		return this.lastName;
+	}
+
+
+}
